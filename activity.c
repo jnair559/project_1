6d2a1d42fd01117c921f4f4855a6e1a87a3a118a
jnair559 <47804021+jnair559@users.noreply.github.com>
2019-02-22 13:05:33 +0530

    Update activity.c

--- activity.c
+++ activity.c
@@ -1,18 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int rpm(int rpm1,int rpm2);
+float p1,p2;
+float ratio;
 int main()
 {
 
-    int i=10;
-    int a[5];
-    int*p;
-    int j=20;
-    int a,b,sum;
-    a=10;
-    b=40;
-    sum = a+b;
-    printf("sum =%d",sum);
-    printf("Value =%d",a[6]);
-   
+    int dia_1=3;//diameter of wheels in meter
+    int dia_2=2;
+    float a=3.14;
+
+    p1=a*dia_1;//calculating perimeter
+    printf("Perimeter1=%.2f \n",p1);
+
+    p2=a*dia_2;
+    printf("Perimeter2=%.2f \n",p2);
+
+    ratio=p1/p2;
+    printf("Ratio=%.1f",ratio);
+
+}
+int rpm(int rpm1,int rpm2)
+{
+    int speed1=1000;//speed of large wheel 60kmph=1000mps
+    int speed2=2000;
+
+    rpm1= (ratio*speed1)/p1;
+    printf("RPM1=%d",rpm1);
+    printf("RPM2=%d",rpm2);
+    rpm2= (ratio*speed2)/p2;
+
 }
