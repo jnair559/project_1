04d05d413ba407c32497f42e577f2b3d819b21e2
jnair559 <47804021+jnair559@users.noreply.github.com>
2019-02-22 12:15:05 +0530

    Update activity.c

--- activity.c
+++ activity.c
@@ -4,8 +4,8 @@
 int main()
 {
 
-    int i;
-    char a[5];
+    int i=10,x;
+    int a[5]=[1 2 3 4 5];
     int*p;
     int j=20;
     int a,b,sum;
@@ -13,4 +13,6 @@ int main()
     b=40;
     sum = a+b;
     printf("sum =%d",sum);
+    printf("Value =%d",a[6]);
+    x=i++*i++;
 }
